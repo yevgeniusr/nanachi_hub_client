51383904d516493423fde61da7e271a89f8ee990
Pifagor <zheka.rachkovan@gmail.com>
2019-09-03 08:25:04 +0300

    Create basic template

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { Router, Route } from "svelte-routing";
+	import NavLink from "./components/NavLink.svelte";
+  	import Hall from "./routes/Hall.svelte";
+	import NotFound from "./routes/NotFound.svelte";
+	import Clicker from "./routes/Clicker.svelte";
+
+	export let url = "";
+</script>
+<main>
+	<p>main</p>
+	<Router url="{url}">
+	<div>
+		<Route path="/clicker" component="{Clicker}" />
+		<Route path="/" component="{Hall}" />
+		<Route path="*" component="{NotFound}" />
+	</div>
+	</Router>
+</main>
+
+
+<style lang="scss">
+	:global(:root) {
+		--base-color: rgb(28, 29, 28);
+	}
+
+	:global(h1) {
+        color: green;
+    }
+
+	main {
+		background-color: var(--base-color);
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
---
+++ src/routes/Hall.svelte
@@ -0,0 +1,11 @@
+<script>
+    import NavLink from '../components/NavLink.svelte'
+
+</script>
+
+<h1>Hall</h1>
+<NavLink to="/clicker">Clicker</NavLink>
+
+<style lang="scss">
+
+</style>
